2cf52e01d06f156ae4e4db0c04005d28fe6446d1
Yatharth0403 <51381659+yatharth0403@users.noreply.github.com>
2020-03-27 11:48:43 +0530

    Data preprocessing using R

---
+++ data_preprocessing_R.R
@@ -0,0 +1,23 @@
+#IMPORTING DATASET
+dataset=read.csv('Data.csv')
+#HANDLING MISSING DATA
+dataset$Age=ifelse(is.na(dataset$Age),ave(dataset$Age,FUN=function(x) mean(x,na.rm=TRUE)),dataset$Age)
+dataset$Salary=ifelse(is.na(dataset$Salary),ave(dataset$Salary,FUN=function(x) mean(x,na.rm=TRUE)),dataset$Salary)
+#HANDLING CATEGORICAL DATA
+dataset$Country=factor(dataset$Country,
+                       levels=c('France','Spain','Germany'),
+                       labels = c(1,2,3))
+dataset$Purchased=factor(dataset$Purchased,
+                         levels=c('No','Yes'),
+                         labels=c(0,1)
+                         )
+#SPLITTING INTO TRAINING AND TEST SET
+library(caTools)
+set.seed(123)
+split=sample.split(dataset$Purchased,SplitRatio = .8)
+training_set=subset(dataset,split==TRUE)
+test_set=subset(dataset,split==FALSE)
+#FEATURE SCALING
+training_set[,2:3]=scale(training_set[,2:3])
+test_set[,2:3]=scale(test_set[,2:3])
+
